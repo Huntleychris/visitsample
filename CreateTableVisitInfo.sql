793189ba0f7b35e476bbb3a44539ff73dcb59a87
Chris Huntley <huntleychristopher@gmail.com>
2022-07-30 09:44:30 -0400

    update visitinfo table

--- CreateTableVisitInfo.sql
+++ CreateTableVisitInfo.sql
@@ -1,3 +1,6 @@
---CREATE TABLE visitinfo (personid int identity (1000000,1) not null primary key, visitstartdatetime datetime, visitenddatetime datetime)
+CREATE TABLE 
+visitinfo 
+(personid int identity (1000000,1) not null primary key, 
+visitstartdatetime datetime, 
+visitenddatetime datetime)
 
-INSERT INTO visitinfo 
