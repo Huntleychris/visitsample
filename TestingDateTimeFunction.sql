484a2e1b41a2e7579759475a1186e85ac98db7ad
Chris Huntley <huntleychristopher@gmail.com>
2022-08-05 08:19:38 -0400

    update of testing for random visit

--- TestingDateTimeFunction.sql
+++ TestingDateTimeFunction.sql
@@ -6,7 +6,14 @@ DECLARE @StartDate DATETIME
 SET @StartDate = DATEADD(yy, DATEDIFF(yy, 0, GETDATE()-365), 0) --Starting value of Date Range Jan 1 the year prior to this one
 DECLARE @EndDate DATETIME 
 SET @EndDate = DATEADD(yy, DATEDIFF(yy, 0, GETDATE()+720), 365) --End Value of Date Range Jan 1 2 years in the futureGO
-
+DECLARE  @StartDateRand DateTime
+DECLARE  @EndDateRand DATETIME 
+DECLARE @RandomDateTime DATETIME
+DECLARE @RandomNumber FLOAT
+DECLARE @IntervalInDays INT ;
+SET @IntervalInDays = CONVERT(INT, @EndDate - @StartDate)
+SET @RandomNumber =  RAND()                   
+SELECT  @RandomDateTime = DATEADD(ss, @RandomNumber * 86400, DATEADD(dd, @RandomNumber * @IntervalInDays, @StartDate))
 
 CREATE TABLE [DateTable] 
     (
@@ -14,10 +21,15 @@ CREATE TABLE [DateTable]
       [Date] DATETIME
     )
 --Let's add some data
-INSERT  INTO [DateTable]
-SELECT DBO.[GenerateRandomDateTime]
-(@startdate,@EndDate)
+INSERT  INTO [DateTable](date)
+VALUES(@RandomDateTime)
 
 
 SELECT * FROM [DateTable]
 GO
+
+
+
+
+
+    
