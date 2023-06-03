8fb0e61ca96b85cf5178b5115da3441461ba90cb
huntleychris <huntleychristopher@gmail.com>
2023-06-03 18:15:52 -0400

    rename files and update readme

---
+++ sp_speedrun.sql
@@ -0,0 +1,25 @@
+
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Christopher Huntley
+-- Create date: 6/3/2023
+-- Description:	The stored procedure containing all the subsequent steps for the project.
+-- =============================================
+CREATE PROCEDURE sp_speedrun
+	-- Add the parameters for the stored procedure here
+-- No parameters yet
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+END
+GO
